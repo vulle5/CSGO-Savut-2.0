74e99e84e55caec83dcd43ca4b630a6323f91ec6
Vulle5 <severitikkanen@gmail.com>
2016-12-13 22:15:39 +0200

    My stats works and minimap smokes are now images and not part of minimap

--- CSGO/platforms/android/assets/www/js/custom.js
+++ CSGO/platforms/android/assets/www/js/custom.js
@@ -1,8 +1,3 @@
-//Go back to previous page
-function goBack() {
-    window.history.back();
-}
-
 //Create panel
 var panel = '<div data-role="panel" id="mypanel" data-position="left" data-display="push"><h1>CSGO Smokes</h1><a href="#stats" data-transition="slide" class="ui-btn ui-corner-all">My Stats</a></div>';
 
@@ -28,39 +23,54 @@ $("#url-1").on("keyup change", function() {
     user_id = this.value;
     console.log(user_id);
 });
+//Checking if form or list is empty
+var $myStats = $('#myStats');
 
-//AJAX call
 $("button").click(function() {
 
+if ($('myStats').not(':empty')){
+    $("#myStats").empty();
+}
 if (user_id.length < 1){
-        alert("User id is empty");
-        return;
+    alert("User id is empty");
+    return;
 } else {
     url = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=18C381D0049CCF7EFF870B99AA8B8C17&steamid=" + user_id;
     console.log(url);
     
-    var $csgoStats = $('#csgoStats');
+    var $myStats = $('#myStats');
     var jsonarray = [];
     
+    
+//AJAX call
     $.ajax({
         type: 'GET',
         url: url,
         dataType: 'json',
+        error: function (request) {
+            alert("No id was found or profile is private");
+            event.preventDefault();
+        },
         success: function(stats) {
-            console.log('success', stats);
-            for (i = 0; i < 10; i++){ 
-            jsonarray.push(JSON.stringify(stats.playerstats.stats[i].value));    
+            console.log('success', stats);       
+            for (i = 0; i < stats.playerstats.stats.length; i++){ 
+                jsonarray.push(JSON.stringify(stats.playerstats.stats[i].name + ": " + stats.playerstats.stats[i].value));
             }
-            console.log(jsonarray);
-       /*  $.each(jsonarray, function(i, stat){
-                $csgoStats.append('<li>name: ' + stat.name + ', value: ' + stat.value + '</li>');            
-            console.log(stats.playerstats.stats[0].name);    
-           });*/
-           //console.log(data.playerstats.stats[i].value);
-           // console.log(data.playerstats.stats[0].value);
-        }   
+            //Convert seconds to hours in total time played
+            var time = jsonarray[2];
+            time = time.slice(20, 27);
+            time = parseInt(time);
+            hours = Math.round(time / 60 / 60);
+            hours = hours.toString();
+            hours = '"total_time_played: ' + hours + ' hours"';
+            //Make li elements with unique id and add the to site;
+            var str = '';
+            for (i = 0; i < jsonarray.length; i++) {
+               str += '<li id=\'li' + i + '\'>' + jsonarray[i] + '</li>';
+            }
+            $('#myStats').append(str);
+            $( "#li2" ).replaceWith('<li id="li2">'+ hours +'</li>');   
+        }
     });
-    //JSON.parse(jsonarray);
-    //console.log(jsonarray);
     }
 });
